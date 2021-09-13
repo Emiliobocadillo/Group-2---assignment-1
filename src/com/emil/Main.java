e79cfa1c6a74c14cd0154fbb4f10652bf1898aa9
Emil Høeg Engelbrechtsen <Emilhoeeg@gmail.com>
2021-09-13 09:13:39 +0200

    two files tutorial

---
+++ src/com/emil/Main.java
@@ -0,0 +1,19 @@
+package com.emil;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String characterName = "Dave";
+        int characterAge = 40;
+
+        System.out.println("There once was a man named " + characterName);
+        System.out.println("he was " + characterAge + " years old. He really liked");
+
+        characterName = "Emil";
+
+        System.out.println("the name " + characterName + " but didn't like being" + characterAge +".");
+        System.out.println("Most of his friends were also " + characterAge + ", and one");
+        System.out.println("of them was also named " + characterName + ".");
+
+    }
+}
